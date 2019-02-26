9dd3362da5d9735b235ab20b4d045d59a5017a75
AxizY <24615015+AxizY@users.noreply.github.com>
2019-02-25 20:27:15 -0500

    Update movement.js

--- movement.js
+++ movement.js
@@ -12,6 +12,7 @@ if(ylocatione == 3){console.log(map.substring(oppoloce)+player+map.substring(loc
 if(ylocatione == 4){console.log(map.substring(oppoloce)+player+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 if(ylocatione == 5){console.log(map.substring(oppoloce)+player+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 }
+/*
 function move() {
 var direct = prompt('Command?', 'Command?')
 if(direct == 'r') {if (oppoloce == 1 || locatione == 6) {} else {
@@ -36,7 +37,36 @@ end()
 
 }} else if(direct == 'close') {return}
 move()}
-
+*/
+var m = {
+  get w() {
+    if (ylocatione == 1) {} else {
+yoppoloce = yoppoloce+1
+ylocatione = ylocatione-1
+end()
+}
+  },
+  get a() {
+    if (oppoloce == 6 || locatione == 1) {} else {
+oppoloce = oppoloce+1
+locatione = locatione-1
+end()
+  }},
+  get s() {
+    if (ylocatione == 5) {} else {
+yoppoloce = yoppoloce-1
+ylocatione = ylocatione+1
+end()
+}
+  },
+  get d() {
+    if (oppoloce == 1 || locatione == 6) {} else {
+oppoloce = oppoloce-1
+locatione = locatione+1
+end()
+}
+  }
+}
 function end() {
 clear()
 
