5a55d0bc0a521e0c93224dc7d3a2c8cbd35a76a0
AxizY <24615015+AxizY@users.noreply.github.com>
2019-07-25 18:53:12 -0400

    updated code

--- main.js
+++ main.js
@@ -3,83 +3,68 @@ map = '######'
 locatione = 1
 oppoloce = 6
 ylocatione = 1
-yoppoloce = 6
+y = 6
+//yoppoloce = 6
 end()
+var x
+window.addEventListener("keydown", checkKeyPress, false);
 }
-var settings = {
-  movingstyle: ['input: w, a s, and d', 'direction: u, d, l, and r'],
+//var settings = {
+  //movingstyle: ['input: w, a s, and d', 'direction: u, d, l, and r'],
  
-}
+//}
 //fix
+//var changesettings = {
+  //changesettings.movingstyle[]
+//}
 
-/*
-function m(side) {
+
+function checkKeyPress(key) {
+  if(key.keyCode == "87") {
+    if (ylocatione == 1) {} else {
+    ylocatione = ylocatione-1
+    end()
+}
+}
+
  
-if (side == 'r') {
-if (oppoloce == 1 || locatione == 6) {} else {
-oppoloce = oppoloce-1
-locatione = locatione+1
-}
-} else if (side == 'l') {
-if (oppoloce == 6 || locatione == 1) {} else {
-oppoloce = oppoloce+1
-locatione = locatione-1
-}
-} else if (side == 'd') {
-if (ylocatione == 5) {} else {
-ylocatione = ylocatione+1
-yoppoloce = yoppoloce-1
-}
-} else if (side == 'u') {
-if (ylocatione == 1) {} else {
-ylocatione = ylocatione-1
-yoppoloce = yoppoloce+1
-}}
-end()
-}
-*/
-
-Object.defineProperty(window, 'w', {
-  get() {
-  if (ylocatione == 1) {} else {
-yoppoloce = yoppoloce+1
-ylocatione = ylocatione-1
-end()
-}}
-  })
-Object.defineProperty(window, 'a', {
-  get() {
-  if (oppoloce == 6 || locatione == 1) {} else {
+ //a
+  
+ if (key.keyCode == "65") {
+   if (oppoloce == 6 || locatione == 1) {} else {
 oppoloce = oppoloce+1
 locatione = locatione-1
 end()
   }
-  }})
-Object.defineProperty(window, 's', {
-  get() {
+ }
+
+    //w
+
+  //s
+ if (key.keyCode == "83") {
 if (ylocatione == 5) {} else {
-yoppoloce = yoppoloce-1
 ylocatione = ylocatione+1
 end()
 }
-  }})
-Object.defineProperty(window, 'd', {
-  get() {
+ }
+
+  //d
+   if (key.keyCode == "68") {
+     
    if (oppoloce == 1 || locatione == 6) {} else {
 oppoloce = oppoloce-1
 locatione = locatione+1
 end()
+  }
+ }
 }
-  }})
 
 
 function end() {
-clear()
-
+console.clear()
 if(ylocatione == 1){console.log(map.substring(oppoloce)+'&'+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 if(ylocatione == 2){console.log(map.substring(oppoloce)+'&'+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 if(ylocatione == 3){console.log(map.substring(oppoloce)+'&'+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 if(ylocatione == 4){console.log(map.substring(oppoloce)+'&'+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 if(ylocatione == 5){console.log(map.substring(oppoloce)+'&'+map.substring(locatione))} else {console.log(map.substring(oppoloce-1)+map.substring(locatione))}
 }
-
